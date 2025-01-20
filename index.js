e6be4dfcc0a2b0be8e1b217323caf70c2c360c59
Sara Rampazzo <sararampazzo.ph@gmail.com>
2025-01-20 23:01:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Cerca tutti gli elementi con classe "paragrafo".
+const $paragrafo = document.getElementsByClassName("paragrafo");
+console.log("Cerca tutti gli elementi con classe 'paragrafo'", $paragrafo);
+
+// Cerca l' elemento con id "elemento-3".
+const $elemento3 = document.getElementById("elemento-3");
+console.log("Cerca l'elemento con id 'elemento-3'", $elemento3);
+
+// Cerca tutti gli elementi h2 della pagina.
+const $elementiH2 = document.querySelectorAll("h2");
+console.log("Cerca tutti gli elementi h2 della pagina", $elementiH2);
