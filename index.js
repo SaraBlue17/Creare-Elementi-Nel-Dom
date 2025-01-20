e71dd7bbc5c6d5f2c5dae480858d79f1501087d8
Sara Rampazzo <sararampazzo.ph@gmail.com>
2025-01-20 23:10:39 +0100

    aggiunti appunti videolezione

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ console.log("Cerca l'elemento con id 'elemento-3'", $elemento3);
 // Cerca tutti gli elementi h2 della pagina.
 const $elementiH2 = document.querySelectorAll("h2");
 console.log("Cerca tutti gli elementi h2 della pagina", $elementiH2);
+
+// Video soluzione
+
+const paragraphs = document.getElementsByClassName("paragrafo");
+console.log(paragraphs);
+// Cerca l'elemento con id "elemento-3"
+const elementId3 = document.getElementById("elemento-3");
+console.log(elementId3);
+// Cerca tutti gli elementi h2 della pagina
+const allTitlesH2 = document.querySelectorAll("h2");
+console.log(allTitlesH2);
